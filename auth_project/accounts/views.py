60073d15772a89b79c5f08fe7c05a36534cd3f5e
EkaMoyak1 <moyekaLogin@gmail.com>
2025-09-05 23:27:44 +0900

    v1

---
+++ auth_project/accounts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.contrib.auth import views as auth_views
+from .forms import CustomUserCreationForm  # Импортируем кастомную форму
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Аккаунт {username} успешно создан! Теперь вы можете войти.')
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'registration/profile.html')
+
+def home(request):
+    return render(request, 'home.html')
